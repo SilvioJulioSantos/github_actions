fdef8b5cb7c601194ac81c05864f1fb40d0386f8
“Silvio <silvio.dossantos@mercadolivre.com>
2022-12-20 15:11:35 -0300

    Projeto atividade CICD desenvolvimento

---
+++ src/test/java/br/generation/meli/com/example/hello/world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.generation.meli.com.example.hello.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
